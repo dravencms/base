6c1a9af325685e8ec4c2699e08f10dc3220c3c00
Adam Schubert <adam.schubert@sg1-game.net>
2016-11-30 11:33:49 +0100

    Added flas type consts

---
+++ src/Dravencms/Flash.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright (C) 2016 Adam Schubert <adam.schubert@sg1-game.net>.
+ */
+
+namespace Dravencms;
+
+
+class Flash
+{
+    const SUCCESS = 'alert-success';
+    const DANGER = 'alert-danger';
+    const WARNING = 'alert-warning';
+    const INFO = 'alert-info';
+}
