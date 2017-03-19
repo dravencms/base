f0bd0a7a8aa2db452528874999e0d98977bad6fa
Adam Schubert <adam.schubert@sg1-game.net>
2017-03-20 00:33:21 +0100

    Added memory debugger

---
+++ src/Dravencms/Debuger.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sadam
+ * Date: 20.3.17
+ * Time: 0:00
+ */
+
+namespace Dravencms;
+
+
+use Tracy\Debugger AS TDebugger;
+
+class Debugger extends TDebugger
+{
+    public static $lastUsage = 0;
+
+    public static function memoryPoint()
+    {
+        $usage = memory_get_usage();
+
+        $convert = function ($size)
+        {
+            $unit = ['b','kb','mb','gb','tb','pb'];
+            return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
+        };
+
+        self::barDump($convert($usage - self::$lastUsage));
+        self::$lastUsage = $usage;
+    }
+}
