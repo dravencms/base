e64468c867ab0c31b5f8e97d4a1f12533a207059
Adam Schubert <adam.schubert@sg1-game.net>
2018-01-06 06:12:56 +0100

    Added columnPosition

--- src/Dravencms/Components/BaseGrid/Grid.php
+++ src/Dravencms/Components/BaseGrid/Grid.php
@@ -6,7 +6,9 @@
 namespace Dravencms\Components\BaseGrid;
 
 use Dravencms\Components\BaseGrid\Column\ColumnBoolean;
+use Dravencms\Components\BaseGrid\Column\ColumnPosition;
 use Dravencms\Components\BaseGrid\Column\PresenterAction;
+use Nette\Utils\Html;
 use Ublaboo\DataGrid\DataGrid;
 
 class Grid extends DataGrid
@@ -22,6 +24,21 @@ class Grid extends DataGrid
         $this->addColumnCheck($key);
         $column = $column ?: $key;
         return $this->addColumn($key, new ColumnBoolean($this, $key, $column, $name));
+    }
+
+    /**
+     * @param $key
+     * @param $name
+     * @param string $upHref
+     * @param string $downHref
+     * @param null $column
+     * @return \Ublaboo\DataGrid\Column\Column
+     * @throws \Ublaboo\DataGrid\Exception\DataGridException
+     */
+    public function addColumnPosition($key, $name, $upHref = 'up!', $downHref = 'down!', $column = null)
+    {
+        $this->addColumnCheck($key);
+        return $this->addColumn($key, new ColumnPosition($this, $key, $column, $name, $upHref, $downHref));
     }
 
     /**
